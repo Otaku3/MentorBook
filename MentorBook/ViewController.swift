828a13eaf6d5e6e905b53dfa49f49cf73d452620
Otaku3 <Otakumi@m.cs.osakafu-u.ac.jp>
2018-06-09 15:59:35 +0900

    finish textbook

--- MentorBook/ViewController.swift
+++ MentorBook/ViewController.swift
@@ -9,15 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var mentorArray: [Mentor] = []
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var courseLabel: UILabel!
+    
+    var index: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        mentorArray.append(Mentor(name: "ビルド", course: "東都", imageName: "build.png"))
+        mentorArray.append(Mentor(name: "クローズ", course: "東都", imageName: "cross-Z.png"))
+        mentorArray.append(Mentor(name: "グリス", course: "北都", imageName: "gris.png"))
+        
+        setUI()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setUI() {
+        imageView.image = mentorArray[index].getImage()
+        nameLabel.text = mentorArray[index].name
+        courseLabel.text = mentorArray[index].course
+    }
+    
+    @IBAction func back() {
+        index -= 1
+        if index == -1 {
+            index = 2
+        }
+        setUI()
+    }
+    
+    @IBAction func next() {
+        index += 1
+        if index == 3 {
+            index = 0
+        }
+        setUI()
     }
 
 
